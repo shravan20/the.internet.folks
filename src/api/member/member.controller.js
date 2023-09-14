906c35e44930ad4f92bd49ba2527ff4c8b597a1b
Shravan <shravankumarb@locus.sh>
2023-09-15 00:23:03 +0530

    Communities and Communities Members deletion done

--- src/api/member/member.controller.js
+++ src/api/member/member.controller.js
@@ -14,16 +14,31 @@ async function createMember(request, response, next) {
         if (validation.fails()) {
             throw new BadRequestError(Object.values(validation.errors.all()).flatMap(i => i));
         }
-        // await service.createMember(request.body);
+        await service.createMember(request.body, request.uid);
         response.success(request.body);
     } catch (error) {
-
+        response.error(error);
     }
 }
 
 
 async function deleteMember(request, response, next) {
+    try {
+        let rules = {
+            "community": "required|string",
+            "user": "required|string",
+            "role": "required|string"
+        }
 
+        let validation = new Validator(request.body, rules);
+        if (validation.fails()) {
+            throw new BadRequestError(Object.values(validation.errors.all()).flatMap(i => i));
+        }
+        await service.deleteMember(request.body, request.uid);
+        response.success(request.body);
+    } catch (error) {
+        response.error(error);
+    }
 }
 
 
--- src/api/member/member.service.js
+++ src/api/member/member.service.js
@@ -24,18 +24,20 @@ async function createMember(data, uid) {
 }
 
 
-async function deleteMember(data) {
+async function deleteMember(data, uid) {
     try {
         let user = userService.getUserById(data.user);
         let community = communityService.getCommunityById(data.community);
         let role = roleService.getRoleById(data.role);
 
         if (uid != community.owner) {
-            throw new BadRequestError("You cannot add the member to this community due to lack of permission");
+            throw new BadRequestError("You cannot delete the member to this community due to lack of permission");
         }
-        let member = await repository.deleteMember({
+
+        await repository.deleteMember({
             community: data.community, user: data.user, role: data.role
         });
+
         return data;
     } catch (error) {
         throw error;
