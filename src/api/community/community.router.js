25e7bbc124bebfec1d2efc88a247fb934474ebd4
Shravan <shravankumarb@locus.sh>
2023-09-16 15:30:47 +0530

    Community flows

--- src/api/community/community.router.js
+++ src/api/community/community.router.js
@@ -4,9 +4,9 @@ const middleware = require("../../middlewares/jwt.middleware");
 
 router.get("/", middleware.tokenVerification, controller.getAllCommunity);
 router.get("/:id/members", middleware.tokenVerification, controller.getAllMembersByCommunity);
-router.get("/me/owner", controller.getMyOwnedCommunity);
-router.get("/me/member", controller.getMyJoinedCommunity);
-router.post("/", controller.createCommunity);
+router.get("/me/owner", middleware.tokenVerification, controller.getMyOwnedCommunity);
+router.get("/me/member", middleware.tokenVerification, controller.getMyJoinedCommunity);
+router.post("/", middleware.tokenVerification, controller.createCommunity);
 
 
 
